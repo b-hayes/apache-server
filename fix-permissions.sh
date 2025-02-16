644b644baef6cf24d74a2b2eaed8594232ca33a7
b-hayes <bradozman@gmail.com>
2025-02-16 23:03:07 +1100

    setting permissions for the www folder

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -3,13 +3,13 @@
 # Set ownership to your user and www-data group
 sudo chown -R $USER:www-data /var/www/
 
-# Set permissions to 775 (rwx for user and group, rx for others)
-sudo chmod -R 775 /var/www/
+# Set permissions for existing files, user and group, but no others.
+sudo chmod -R 770 /var/www/
 
 # Set setgid bit to inherit group ownership for new files
 sudo chmod -R g+s /var/www/
 
-# Set default permissions for new files
-sudo setfacl -d -m u:$USER:rwx /var/www/
-sudo setfacl -d -m g:www-data:rwx /var/www/
-sudo setfacl -d -m o:--- /var/www/
+# Set default permissions for new directories
+sudo setfacl -d -R -m u:$USER:rwx /var/www/
+sudo setfacl -d -R -m g:www-data:rwx /var/www/
+sudo setfacl -d -R -m o:--- /var/www/
